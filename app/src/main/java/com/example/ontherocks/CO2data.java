c966b6f17ef35d8522d30d74f264f90c6a3f92df
Erturan <47361021+Erturan@users.noreply.github.com>
2021-03-13 23:18:36 +0000

    Now print co2 emissions and co2 costs

--- app/src/main/java/com/example/ontherocks/CO2data.java
+++ app/src/main/java/com/example/ontherocks/CO2data.java
@@ -2,30 +2,31 @@ package com.example.ontherocks;
 
 public class CO2data {
 
-    final double kmtomiles = 3956.0/6371.0;
-    final double car = 0.3;
-    final double train = 0.09;
-    final double bus = 0.15;
-    final double rate = 80;
+    final static double kmtomiles = 3956.0/6371.0;
+    final static double car = 0.3;
+    final static double train = 0.09;
+    final static double bus = 0.15;
+    //Rate per tonne of c02
+    final static double rate = 80;
 
     // in kg
-    public double carEmission (double dist) {
+    public static double carEmission (double dist) {
         return dist*kmtomiles*car;
     }
 
     // in kg
-    public double busEmission (double dist) {
+    public static double busEmission (double dist) {
         return dist*kmtomiles*bus;
     }
 
     // in kg
-    public double trainEmission (double dist) {
+    public static double trainEmission (double dist) {
         return dist*kmtomiles*train;
     }
 
 
-    public double cost (double kgemission) {
-        return kgemission*rate/10000;
+    public static double cost (double kgemission) {
+        return kgemission*rate/1000;
     }
 
 
