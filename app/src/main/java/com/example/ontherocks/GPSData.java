289f66cdd9d5419f11a54b416925cda79c708f68
pulkitaggarwal <pulkit3012@gmail.com>
2021-03-13 23:31:49 +0400

    can calculate the distance between given gps coordinates

--- app/src/main/java/com/example/ontherocks/GPSData.java
+++ app/src/main/java/com/example/ontherocks/GPSData.java
@@ -12,6 +12,28 @@ public class GPSData {
         this.Longitude = longitude;
     }
 
+
+    public double distance(GPSData a, GPSData b) {
+        double lat1 = Math.toRadians(a.Latitude);
+        double lat2 = Math.toRadians(b.Latitude);
+        double lon1 = Math.toRadians(a.Longitude);
+        double lon2 = Math.toRadians(b.Longitude);
+
+        double dlon = lon2 - lon1;
+        double dlat = lat2 - lat1;
+        double dist = Math.pow(Math.sin(dlat / 2), 2)
+                + Math.cos(lat1) * Math.cos(lat2)
+                * Math.pow(Math.sin(dlon / 2),2);
+
+        double c = 2 * Math.asin(Math.sqrt(dist));
+
+        // for Kilometers. For miles use 3956
+        double r = 6371;
+
+        return(c * r);
+    }
+
+
     @Override
     public String toString() {
         return "Latitude: " + Latitude + " " + "Longitude" + Longitude;
